c29839a2a0c9501b78670dcbfd9a3782fe6bfb9b
riteshrana12-dev <ranaritesh677@gmail.com>
2026-08-16 15:59:54 +0530

    test: ignore build artifacts and extend timeout for integration runs

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,6 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   setupFiles: ["dotenv/config"],
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+  testTimeout: 20000,
 };
